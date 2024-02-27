ce23d95aa87861112432e8e5129f1814493ceb01
Marius Tresoldi <tremared@gmail.com>
2024-02-27 11:49:38 +0100

    switched to keras numpy mnist instead of tensorflow_datasets mnist to make data augmentation easier

--- mnist_flax_tutorial.py
+++ mnist_flax_tutorial.py
@@ -2,8 +2,8 @@
 The following script is a copy-paste of the MNIST flax tutorial: https://flax.readthedocs.io/en/latest/quick_start.html
 Some personal comments were added for understanding.
 TODO:
-- use keras jnp version of mnist
-- implement data augmentation
+- use keras jnp version of mnist to make data augmentation easier
+- implement Heinze data augmentation
 - implement conditional variance regularization
 """
 
@@ -17,10 +17,11 @@ from flax.training import train_state
 from flax import struct
 import optax
 import matplotlib.pyplot as plt
+import keras
 
 
+"""
 def get_datasets(num_epochs, batch_size):
-    """Load MNIST train and test datasets into memory."""
 
     train_ds = tfds.load('mnist', split='train', data_dir='.')
     test_ds = tfds.load('mnist', split='test')
@@ -44,6 +45,7 @@ def get_datasets(num_epochs, batch_size):
     test_ds = test_ds.batch(batch_size, drop_remainder=True).prefetch(1)
 
     return train_ds, test_ds
+"""
 
 
 class CNN(nn.Module):
@@ -76,6 +78,8 @@ class Metrics(metrics.Collection):
 
 # it is not readable here but the training state will actually consist of:
 # a forward pass function, model parameters, an optimizer and values of the metrics chosen above
+
+
 class TrainState(train_state.TrainState):
     metrics: Metrics
 
@@ -83,27 +87,29 @@ class TrainState(train_state.TrainState):
 def create_train_state(module, rng, learning_rate):
     """Creates an initial `TrainState`."""
     # mnist is 28x28
-    params = module.init(rng, jnp.ones([1, 28, 28, 1]))['params']
+    _params = module.init(rng, jnp.ones([1, 28, 28, 1]))['params']
 
     tx = optax.adam(learning_rate)
 
-    return TrainState.create(apply_fn=module.apply, params=params, tx=tx, metrics=Metrics.empty())
+    return TrainState.create(apply_fn=module.apply, params=_params, tx=tx, metrics=Metrics.empty())
 
 # uncomment jit to see values while debugging
+
+
 @jax.jit
-def train_step(state, batch):
+def train_step(state, images, labels):
     """Train for a single step."""
 
-    def loss_fn(params):
+    def loss_fn(params_):
 
         # unclear why one needs to pass the params as single key dictionary, can't find anything in the internet
         # batch_size * 10 array
-        logits = state.apply_fn({'params': params}, batch['image'])
+        logits = state.apply_fn({'params': params_}, images)
 
         # TODO: include conditional variance penalty
         # scalar
-        loss = optax.softmax_cross_entropy_with_integer_labels(logits=logits, labels=batch['label']).mean()
-        
+        loss = optax.softmax_cross_entropy_with_integer_labels(logits=logits, labels=labels).mean()
+
         return loss
 
     grad_fn = jax.grad(loss_fn)
@@ -113,42 +119,62 @@ def train_step(state, batch):
     return state
 
 # uncomment jit to see values while debugging
+
+
 @jax.jit
-def compute_metrics(state, batch):
+def compute_metrics(state, images, labels):
 
     # unclear why one needs to pass the params as single key dictionary, can't find anything in the internet
     # batch_size x 10 array
-    logits = state.apply_fn({'params': state.params}, batch['image'])
+    logits = state.apply_fn({'params': state.params}, images)
 
     # TODO: include conditional variance penalty
     # scalar
-    loss = optax.softmax_cross_entropy_with_integer_labels(logits=logits, labels=batch['label']).mean()
-    
+    loss = optax.softmax_cross_entropy_with_integer_labels(
+        logits=logits, labels=labels).mean()
+
     # can't find any documentation on what "single_from_model_output" does except for literal source code
-    metric_updates = state.metrics.single_from_model_output(logits=logits, labels=batch['label'], loss=loss)
+    metric_updates = state.metrics.single_from_model_output(logits=logits, labels=labels, loss=loss)
     metrics = state.metrics.merge(metric_updates)
     state = state.replace(metrics=metrics)
 
     return state
 
 
+@jax.jit
+def pred_step(state, images):
+    logits = state.apply_fn({'params': state.params}, images)
+    return logits.argmax(axis=1)
+
+
 if __name__ == "__main__":
 
-    num_epochs = 10
-    batch_size = 32
-    learning_rate = 0.01
-    momentum = 0.9
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+    # TODO: shuffle the data
+    x_train = jnp.array(x_train) / 255
+    x_test = jnp.array(x_test) / 255
+    x_train = jnp.reshape(x_train, (60000, 28, 28, 1))
+    x_test = jnp.reshape(x_test, (10000, 28, 28, 1))
 
-    train_ds, test_ds = get_datasets(num_epochs, batch_size)
+    y_train = jnp.array(y_train).astype(jnp.int32)
+    y_test = jnp.array(y_test).astype(jnp.int32)
+
+    
+    
+
+    num_epochs = 8
+    batch_size = 32
+    learning_rate = 0.005
+
+    # train_ds, test_ds = get_datasets(num_epochs, batch_size)
 
     tf.random.set_seed(0)
     init_rng = jax.random.key(0)
     cnn = CNN()
     state = create_train_state(cnn, init_rng, learning_rate)
 
-    # since train_ds is replicated num_epochs times in get_datasets(), we divide by num_epochs
-    #note: train_ds contains batches not individual data points
-    steps_per_epoch = train_ds.cardinality().numpy() // num_epochs
+    steps_per_epoch = jnp.ceil(jnp.shape(y_train)[0] / batch_size)
+    steps_per_epoch = int(steps_per_epoch)
 
     # metrics_history will contain the relevant metrics after each training epoch
     metrics_history = {'train_loss': [],
@@ -156,13 +182,18 @@ if __name__ == "__main__":
                        'test_loss': [],
                        'test_accuracy': []}
 
-    for step, batch in enumerate(train_ds.as_numpy_iterator()):
+    for step in range(num_epochs * steps_per_epoch):
 
-        state = train_step(state, batch)
-        state = compute_metrics(state=state, batch=batch)
+        i = step % steps_per_epoch
+
+        train_images = x_train[i*batch_size:(i+1)*batch_size, :, :, :]
+        train_labels = y_train[i*batch_size:(i+1)*batch_size]
+
+        state = train_step(state, train_images, train_labels)
+        state = compute_metrics(state, train_images, train_labels)
 
         if (step + 1) % steps_per_epoch == 0:
-            
+
             # again: cant find any info on what metrics.compute() actually does except source code.
             # from source code I gather that it performs averaging s.t. it averages over the metrics
             # of all batches in that epoch
@@ -175,9 +206,10 @@ if __name__ == "__main__":
             # Compute metrics on the test set after each training epoch
             # need to make a copy of the current training state because the saved metrics will be overwritten
             test_state = state
+            test_state = compute_metrics(test_state, x_test, y_test)
             # test set is also fed in batches, I think this should also work by just feeding the entire thing at once
-            for test_batch in test_ds.as_numpy_iterator():
-                test_state = compute_metrics(state=test_state, batch=test_batch)
+            # for test_batch in test_ds.as_numpy_iterator():
+            #    test_state = compute_metrics(state=test_state, batch=test_batch)
 
             for metric, value in test_state.metrics.compute().items():
                 metrics_history[f'test_{metric}'].append(value)
@@ -205,12 +237,4 @@ if __name__ == "__main__":
     plt.show()
     plt.clf()
 
-    @jax.jit
-    def pred_step(state, batch):
-        logits = state.apply_fn({'params': state.params}, test_batch['image'])
-        return logits.argmax(axis=1)
-
-    test_batch = test_ds.as_numpy_iterator().next()
-    pred = pred_step(state, test_batch)
-
-
+    pred = pred_step(state, x_test)
